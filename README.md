ebc8a0c77077585c0e9da5e811e955a2eb64a160
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-08-10 13:53:52 +0100

    Project Initialisation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # uNclear-MenuSystem
- 
+ Multiplayer Plugin made in Unreal Engine 5.2.0 as part of my Mastered bootcamp.
---
+++ Source/uNclearMenuSystem/uNclearMenuSystemGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "uNclearMenuSystemGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AuNclearMenuSystemGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AuNclearMenuSystemGameMode();
+};
+
+
+
