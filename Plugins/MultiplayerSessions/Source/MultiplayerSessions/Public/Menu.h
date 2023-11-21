fdc660079d141188de2ffb1f414ba4f642983100
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-11-21 18:56:14 +0000

    Accessing our Subsystem

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
@@ -13,7 +13,30 @@ UCLASS()
 class MULTIPLAYERSESSIONS_API UMenu : public UUserWidget
 {
 	GENERATED_BODY()
+	
 public:
+	
 	UFUNCTION(BlueprintCallable)
 	void MenuSetup();
+	
+protected:
+
+	virtual bool Initialize() override;
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* HostButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* JoinButton;
+
+	UFUNCTION()
+	void HostButtonClicked();
+
+	UFUNCTION()
+	void JoinButtonClicked();
+
+	// The subsystem designed to handle all online session functionality
+	class UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
 };
