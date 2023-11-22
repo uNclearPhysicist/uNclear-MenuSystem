3c5e1a5d790253b974bd1c11dae38c83d0d84146
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-11-22 19:28:09 +0000

    Tracking Incoming Players

--- Source/uNclearMenuSystem/uNclearMenuSystemCharacter.cpp
+++ Source/uNclearMenuSystem/uNclearMenuSystemCharacter.cpp
@@ -59,16 +59,6 @@ AuNclearMenuSystemCharacter::AuNclearMenuSystemCharacter():
 	if (OnlineSubsystem)
 	{
 		OnlineSessionInterface = OnlineSubsystem->GetSessionInterface();
-
-		if (GEngine)
-		{
-			GEngine->AddOnScreenDebugMessage(
-				-1,
-				15.f,
-				FColor::Blue,
-				FString::Printf(TEXT("Found subsystem %s"), *OnlineSubsystem->GetSubsystemName().ToString())
-			);
-		}
 	}
 }
 
