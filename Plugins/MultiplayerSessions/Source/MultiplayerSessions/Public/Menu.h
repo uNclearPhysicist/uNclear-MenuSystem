9c0928de27077f36484377515dd83292d89462e6
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-11-22 20:34:40 +0000

    Fixing LobbyPath

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
@@ -18,7 +18,7 @@ class MULTIPLAYERSESSIONS_API UMenu : public UUserWidget
 public:
 	
 	UFUNCTION(BlueprintCallable)
-	void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")), FString LobbyPath = FString(TEXT("/Game/ThirdPersonCPP/Maps/Lobby")));
+	void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")), FString LobbyPath = FString(TEXT("/Game/ThirdPerson/Maps/Lobby")));
 	
 protected:
 
